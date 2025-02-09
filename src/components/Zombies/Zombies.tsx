03910e67ab981fc1912876e164593afacf8a6730
Mathew Carter <mathewacarter@gmail.com>
2025-02-08 22:21:42 -0500

    make zombies invisible on click

--- src/components/Zombies/Zombies.tsx
+++ src/components/Zombies/Zombies.tsx
@@ -1,39 +1,71 @@
-import zombie from "../../assets/zombie.png";
+import zombieImage from "../../assets/zombie.png";
+import { useState } from "react";
 
 function Zombies() {
+  const NUM_ZOMBIES = 10;
+  type zombieSettings = {
+    index: number;
+    visible: boolean;
+    x: number;
+    y: number;
+  };
+
+  const [zombies, setZombies] = useState<zombieSettings[]>(
+    initZombies(NUM_ZOMBIES)
+  );
+
   function getRandomIntInclusive(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  const NUM_ZOMBIES = 10;
-  function renderZombies(numZombies: number) {
-    let arr = [];
-    for (let i = 0; i < numZombies; i++) {
+  function initZombies(num: number): zombieSettings[] {
+    let arr: zombieSettings[] = [];
+    for (let i = 0; i < num; i++) {
       const x = getRandomIntInclusive(10, 90);
       const y = getRandomIntInclusive(10, 90);
-      const image = (
-        <img
-          key={i}
-          src={zombie}
-          style={{
-            width: 90,
-            height: 90,
-            position: "absolute",
-            left: x + "%",
-            top: y + "%",
-          }}
-          onClick={() => zombieClick()}
-        />
-      );
-      arr.push(image);
+      const setting: zombieSettings = {
+        index: i,
+        x,
+        y,
+        visible: true,
+      };
+      arr.push(setting);
     }
     return arr;
   }
 
-  function zombieClick() {
-    console.log("clicked");
+  function zombieClick(index: number) {
+    setZombies(
+      zombies.map((zombie, i) => {
+        if (i === index) {
+          return { ...zombie, visible: false };
+        }
+        return zombie;
+      })
+    );
   }
-  return <div>{renderZombies(NUM_ZOMBIES)}</div>;
+
+  return (
+    <div>
+      {zombies.map((zombie) => {
+        return (
+          <img
+            key={zombie.index}
+            src={zombieImage}
+            style={{
+              width: 90,
+              height: 90,
+              position: "absolute",
+              left: zombie.x + "%",
+              top: zombie.y + "%",
+              visibility: zombie.visible ? "visible" : "hidden",
+            }}
+            onClick={() => zombieClick(zombie.index)}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Zombies;
